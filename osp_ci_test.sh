9fc94bab854dac4d3de3af403d5459377b636556
Asma Syed Hameed <asyedham@redhat.com>
2019-12-24 14:56:39 +0530

    add changes5

--- osp_ci_test.sh
+++ osp_ci_test.sh
@@ -12,22 +12,21 @@ ssh -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_
   source stackrc
   cd ~/stockpile_job_osp
   pushd ~/stockpile_job_osp
-  # Prep the results.markdown file
-  echo "Results for "$JOB_NAME > results.markdown
-  echo "" >> results.markdown
+  # Prep the results_osp.markdown file
+  echo "Results for stockpile_osp_job" > results_osp.markdown
+  echo "" >> results_osp.markdown
   echo 'Tag | SubTag | ok | changed | unreachable | failed | Returned info' >> results.markdown
   echo '-----|------- |---|---------|-------------|--------|--------' >> results.markdown
   # Setup hosts and vars for CI environment
   cp ci/all.yml group_vars/all.yml
-  # Get the list of tags from stockpile.yml (minus lines beginning with #)
   tag_list=`grep tags config/featureset001.yml | grep -v '^ *#'| awk '{print $(NF-1)}'`
   echo $tag_list
   array=("compute.*" "controller.*" "undercloud.*")
   for tag in $tag_list
   do
-     echo $tag
+     figlet $tag
      for i in "${array[@]}"; do
-       results=`ansible-playbook -i ci/hosts config/featureset001.yml --tags=$tag | grep "$i ok=.*changed=.*unreachable=.*failed=.*"`
+       results=`ansible-playbook -i ci/hosts config/featureset001.yml --tags=$tag,dump-facts | grep "$i ok=.*changed=.*unreachable=.*failed=.*"`
        echo $results
        # set resulting variables
        res=`echo $results | sed 's/=/ /g'`
@@ -37,14 +36,14 @@ ssh -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_
        unreachable=`echo $res | awk '{print $8}'`
        failed=`echo $res | awk '{print $10}'`
        check_json=`python ci/check_json.py -i /tmp/stockpile.json`
-       echo "Results from: "$tag
+       echo "Results from: stockpile_osp_job"
        if [[ -z $check_json ]]
        then
          echo "Returned no information"
-         echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned NO information" >> results.markdown
+         echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned NO information" >> results_osp.markdown
        else
          echo "Returned valid JSON information"
-         echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned valid JSON information" >> results.markdown
+         echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned valid JSON information" >> results_osp.markdown
        fi
     echo ""
     done

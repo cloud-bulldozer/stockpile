c0e8ababd464f93b7ad3884ec71f75145925acdd
asyedham <asyedham@redhat.com>
2020-01-14 12:04:38 -0500

    Add ci job script Fix the check_json for multiple keys

--- osp_ci_test.sh
+++ osp_ci_test.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 
-#copy the stockpile in the undercloud
 JENKINSDIR=${jenkins_dir}
 BASEDIR="/home/stockpile/ospci"
 VENVDIR="$BASEDIR/venv"
@@ -21,31 +20,42 @@ echo 'Tag | SubTag | ok | changed | unreachable | failed | Returned info' >> res
 echo '-----|------- |---|---------|-------------|--------|--------' >> results_osp.markdown
 # Setup hosts and vars for CI environment
 cp ci/all.yml group_vars/all.yml
-tag_list=`grep tags config/featureset001.yml | grep -v '^ *#'| awk '{print $(NF-1)}'`
-echo $tag_list
-array=("compute.*" "controller.*" "undercloud.*")
-for tag in $tag_list
+labels=("oc_*" "uc_*")
+for label in ${labels[@]}
 do
-  figlet $tag
-  for i in "${array[@]}"; do
-    results=`ansible-playbook -i ci/hosts config/featureset001.yml --tags=$tag,dump-facts | grep "$i ok=.*changed=.*unreachable=.*failed=.*"`
-    echo $results
-    # set resulting variables
-    res=`echo $results | sed 's/=/ /g'`
-    subtag=`echo $res | awk '{print $1}'`
-    ok=`echo $res | awk '{print $4}'`
-    changed=`echo $res | awk '{print $6}'`
-    unreachable=`echo $res | awk '{print $8}'`
-    failed=`echo $res | awk '{print $10}'`
-    check_json=`python ci/check_json.py -i /tmp/stockpile.json`
-    echo "Results from: stockpile_osp_job"
-    if [[ -z $check_json ]]; then
-      echo "Returned no information"
-      echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned NO information" >> results_osp.markdown
-    else
-      echo "Returned valid JSON information"
-      echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned valid JSON information" >> results_osp.markdown
-    fi
-  echo ""
+  tag_list=`grep 'tags: '"$label"'' config/featureset001.yml | grep -v '^ *#'| awk '{print $(NF-1)}'`
+  echo $tag_list
+  if [ $label = "oc_*" ]; then
+    array=("controller.*" "compute.*")
+  elif [ $label = "uc_*" ]; then 
+    array=("undercloud.*")
+  else
+    pass
+  fi
+  for tag in $tag_list
+  do
+    figlet $tag
+    for i in "${array[@]}"; do
+      results=`ansible-playbook -i ci/hosts config/featureset001.yml  -v --tags=openstack_common,$tag,dump-facts | grep "$i ok=.*changed=.*unreachable=.*failed=.*"`
+      echo $results
+      # set resulting variables
+      res=`echo $results | sed 's/=/ /g'`
+      subtag=`echo $res | awk '{print $1}'`
+      ok=`echo $res | awk '{print $4}'`
+      changed=`echo $res | awk '{print $6}'`
+      unreachable=`echo $res | awk '{print $8}'`
+      failed=`echo $res | awk '{print $10}'`  
+      check_json=`python ci/check_json.py -i /tmp/stockpile.json`
+      echo "Results from: stockpile_osp_job"
+      if [[ -z $check_json ]]; then
+        echo "Returned no information"
+        echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned NO information" >> results_osp.markdown
+      else
+        echo "Returned Valid json"
+        echo $tag"|"$subtag"|"$ok"|"$changed"|"$unreachable"|"$failed"|Returned valid JSON information" >> results_osp.markdown
+      fi
+    echo""
+    done
+    echo $check_json
   done
 done
